c55f77a1c14529e64b030452043e9a99414d57a5
thehazyobserver <114879476+thehazyobserver@users.noreply.github.com>
2025-08-29 15:47:15 -0400

    Remove Footer component import and usage to fix build error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Navbar from './components/Navbar'
 import GameDashboard from './components/GameDashboard'
-import Footer from './components/Footer'
 import { config } from './config/wagmi'
 
 const queryClient = new QueryClient()
@@ -14,7 +13,6 @@ function App() {
         <div className="min-h-screen ocean-gradient">
           <Navbar />
           <GameDashboard />
-          <Footer />
         </div>
       </QueryClientProvider>
     </WagmiProvider>
